ea58db6bedc587c879453c766d812fe86dcc1649
bartswen <bartswen@yahoo.com>
2016-02-03 21:03:11 +0100

    Ch14 Pattern exercises

--- scalademo/src/main/scala/bs/scalaimpatient/Chapter14PatternMatchingExercises.scala
+++ scalademo/src/main/scala/bs/scalaimpatient/Chapter14PatternMatchingExercises.scala
@@ -55,31 +55,101 @@ object Chapter14PatternMatchingExercises extends App {
   println { leafSum(tree) }
 
   // 6. binary tree
-  println("6. binary tree")
-  sealed abstract class BinaryTree
-  case class Leaf(value: Int) extends BinaryTree
-  case class Node(left: BinaryTree, right: BinaryTree) extends BinaryTree
+  println("6. binary tree");
+  {
+    sealed abstract class BinaryTree
+    case class Leaf(value: Int) extends BinaryTree
+    case class Node(left: BinaryTree, right: BinaryTree) extends BinaryTree
 
-  def sumTreev1(tree: BinaryTree): Int = {
-    tree match {
-      case l: Leaf => l.value
-      case n: Node => sumTreev1(n.left) + sumTreev1(n.right)
-      case _       => 0
+    def leafSum(tree: BinaryTree): Int = {
+      tree match {
+        case l: Leaf => l.value
+        case n: Node => leafSum(n.left) + leafSum(n.right)
+        case _       => 0
+      }
     }
+
+    val bintree = Node(Node(Leaf(3), Leaf(8)), Node(Leaf(2), Leaf(5)))
+    println { leafSum(bintree) }
+
+    def leafSumv2(tree: BinaryTree): Int = {
+      tree match {
+        case Leaf(v)    => v
+        case Node(l, r) => leafSumv2(l) + leafSumv2(r)
+        case _          => 0
+      }
+    }
+    println { leafSumv2(bintree) }
   }
 
-  val bintree = Node(Node(Leaf(3), Leaf(8)), Node(Leaf(2), Leaf(5)))
-  println { sumTreev1(bintree) }
+  // 7. bintree children unlimited
+  println("7. bintree children unlimited");
+  {
+    sealed abstract class BinaryTree
+    case class Leaf(value: Int) extends BinaryTree
+    case class Node(children: BinaryTree*) extends BinaryTree
 
-  def sumTreev2(tree: BinaryTree): Int = {
-    tree match {
-      case Leaf(v)    => v
-      case Node(l, r) => sumTreev2(l) + sumTreev2(r)
-      case _          => 0
+    def leafSum(tree: BinaryTree): Int = {
+      tree match {
+        case Leaf(v)           => v
+        case Node(childs @ _*) => childs.map(leafSum(_)).foldLeft(0)(_ + _)
+        case _                 => 0
+      }
     }
+
+    val bintree = Node(Node(Leaf(3), Leaf(8)), Leaf(2), Node(Leaf(5)))
+    println { leafSum(bintree) }
   }
-  println { sumTreev2(bintree) }
-  
-  // 7. 
+
+  // 8. bintree using operator
+  println("8. bintree using operator");
+  {
+    sealed abstract class BinaryTree
+    case class Leaf(value: Int) extends BinaryTree
+    case class Node(operator: Char, children: BinaryTree*) extends BinaryTree
+
+    def eval(tree: BinaryTree): Int = {
+      tree match {
+        case Leaf(v) => v
+        case Node(op, childs @ _*) => op match {
+          case '+' => childs.foldLeft(0)(_ + eval(_))
+          case '-' => childs.foldLeft(0)(_ - eval(_))
+          case '*' => childs.foldLeft(1)(_ * eval(_))
+          case _   => 1
+        }
+      }
+    }
+
+    val opTree = Node('+', Node('*', Leaf(3), Leaf(8)), Leaf(2), Node('-', Leaf(5)))
+    println { eval(opTree) }
+
+    // 9. compute sum of non-nodes
+    println("9. compute sum of non-nodes");
+
+    def leafSum(tree: BinaryTree): Int = {
+      if (tree.isInstanceOf[Node])
+        tree.asInstanceOf[Node].children.foldLeft(0)(_ + leafSum(_))
+      else
+        tree.asInstanceOf[Leaf].value
+    }
+
+    println(leafSum(opTree))
+  }
+
+  // 10. compose functions
+  println("10. compose functions")
+  type T = Double => Option[Double]
+  def compose(l: T, r: T): T = {
+    (x: Double) => r(x).flatMap(l(_)) // must apply r first to get to the results as in the example
+  }
+
+  def f(x: Double) = if (x >= 0) Some(math.sqrt(x)) else None
+  def g(x: Double) = if (x != 1) Some(1 / (x - 1)) else None
+  def h = compose(f, g)
+
+  println(h(2)) // Some(1.0)
+  println(h(1)) // None
+  println(h(0)) // None
+  println(h(-1)) // None
 
 }
